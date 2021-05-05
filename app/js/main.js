ce4880c17f07539fee3b0c97228cc2038e450fe0
scvara <scvara@internet.ru>
2021-05-05 15:00:41 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+    
+    var mixer = mixitup('.products__inner-box');
+
+
 });
